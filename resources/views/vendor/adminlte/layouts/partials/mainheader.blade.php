18a259eaf70f79b98cc82193fcbb3fb6b71eb063
Mazbaul <mazbaul17@gmail.com>
2018-02-21 14:52:09 +0600

    laravel

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -6,7 +6,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>A</b>LT</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Admin</b>LTE Laravel </span>
+        <span class="logo-lg">Admin<b> DASHBOARD </b></span>
     </a>
 
     <!-- Header Navbar -->
